156d66e5ed933230a00a47798e7cfd3146d59a72
Aman Bhardwaj <inblack>
2021-02-15 12:09:19 +0530

    redis is here

--- cache/cache.go
+++ cache/cache.go
@@ -1,18 +1,36 @@
 package cache
 
 import (
+	"context"
 	"fmt"
+	"log"
 
 	"github.com/go-redis/redis/v8"
 )
 
 // StartRedis ...
 func StartRedis() (*redis.Client){
-	redis := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-        Password: "", // no password set
-        DB:       0,  // use default DB
-	})
+	redis := redis.NewClient(&redis.Options{})
 	fmt.Println("Redis is here")
+
+	ctx := context.Background()
+
+	redis.FlushAll(ctx)
+
+	err := redis.Set(ctx, "hello", "worlds", 0).Err()
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	value, err2 := redis.Get(ctx, "hello").Result()
+
+	if err2 != nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println("===========", value)
+
+	defer redis.Close()
+
 	return redis
 }
